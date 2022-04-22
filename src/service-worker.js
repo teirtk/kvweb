f753e12f9758def4f070936b66ddde58cf8eeec4
teirtk <teirtboo@gmail.com>
2022-04-22 16:41:34 +0700

    update

--- src/service-worker.js
+++ src/service-worker.js
@@ -3,10 +3,24 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.1.5/workbox
 
 // This will work!
 workbox.routing.registerRoute(
-  ({request}) => request.destination === 'image',
-  new workbox.strategies.StaleWhileRevalidate()
+  ({ request }) =>
+  request.destination === 'image' ||
+    request.destination === 'style' ||
+    request.destination === 'script' ||
+    request.destination === 'worker',
+  // Use a Stale While Revalidate caching strategy
+  new workbox.strategies.StaleWhileRevalidate({
+    // Put all cached files in a cache named 'assets'
+    cacheName: 'assets',
+    plugins: [
+      // Ensure that only requests that result in a 200 status are cached
+      new workbox.cacheableResponse.Plugin({
+        statuses: [200],
+      }),
+    ],
+  }),
 );
 workbox.routing.registerRoute(
-    ({url}) => url.pathname === '/api',
-    new workbox.strategies.NetworkFirst()
+  ({ url }) => url.pathname.startsWith('/api'),
+  new workbox.strategies.NetworkFirst()
 );
